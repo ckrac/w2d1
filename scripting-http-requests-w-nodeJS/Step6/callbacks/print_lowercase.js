6429eeb0b6cfc0b2a1ee9592427a81a57b5f0074
vagrant <kennethracadio@hotmail.com>
2018-01-22 19:32:41 +0000

    Scripting HTTP Requests with the Request Package

--- scripting-http-requests-w-nodeJS/Step6/callbacks/print_lowercase.js
+++ scripting-http-requests-w-nodeJS/Step6/callbacks/print_lowercase.js
@@ -3,6 +3,7 @@ var getHTML = require('./http-functions');
 var requestOptions = {
   host: 'sytantris.github.io',
   path: '/http-examples/step6/lowercase.html'
+
 };
 
 function printLowerCase (html) {
